7606359c34ba38031bfe109ec45bbe3bd93da016
amishas157 <amishas157@gmail.com>
2018-06-03 15:54:08 +0530

    use with statement for file opening

--- osm-fudge/__main__.py
+++ osm-fudge/__main__.py
@@ -1,16 +1,16 @@
 import bk_tree
 import sys
 
-file  = open(sys.argv[1], 'r')
+with open(sys.argv[1], 'r') as file:
 
-initial_word = file.readline()
-tree_obj = bk_tree.BKTree(initial_word)
+	initial_word = file.readline()
+	tree_obj = bk_tree.BKTree(initial_word)
 
-for line in file: 
-	if len(line) > 1:
-		tree_obj.insert(line.strip(), tree_obj.tree)
+	for line in file: 
+		if len(line) > 1:
+			tree_obj.insert(line.strip(), tree_obj.tree)
 
-results = []
-tree_obj.lookup('parking', tree_obj.tree, 8, results)
+	results = []
+	tree_obj.lookup('parking', tree_obj.tree, 8, results)
 
-print(results)
+	print(results)
