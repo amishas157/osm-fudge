8ccccc08fb6edcd828444fc05881711c92d18baa
Amisha Singla <amishas157@gmail.com>
2018-06-09 02:48:07 +0530

    fix

--- osm_fudge/metrics/jaccard.py
+++ osm_fudge/metrics/jaccard.py
@@ -6,7 +6,7 @@ More: https://en.wikipedia.org/wiki/Jaccard_index
 '''
 
 import argparse
-from osm_fudge.metrics import n_gram
+import n_gram
 
 def find_jaccard_similarity(str1, str2, options):
     a = n_gram.find_n_grams(options['n'], str1)
