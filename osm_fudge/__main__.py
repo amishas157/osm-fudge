f7c5bd1d6bbab5e7208996bc09b6cd33759181e1
Amisha Singla <amishas157@gmail.com>
2018-07-01 21:59:43 +0530

    every param from cli

--- osm_fudge/__main__.py
+++ osm_fudge/__main__.py
@@ -28,10 +28,13 @@ def main():
     parser.add_argument('--max-distance', type=int, required=True,
                         help='Inter string distance allowed for potenital matches')
     parser.add_argument('--max-results', type=int, help='Maximum number of results required')
+    parser.add_argument('--n-gram', type=int, help='n to be used for finding n-grams in jaccard similarity metrics')
+    parser.add_argument('--query-strings', type=str, help='Comma delimited list of strings to be queried')
     args = parser.parse_args()
     with open(args.input, 'r') as file:
         metrics = args.metrics
-        options = {'n': 2}
+        if args.n_gram:
+            options = {'n': args.n_gram}
         tree_obj = bk_tree.BKTree(distance_metrics_method(metrics, options))
 
         for line in file:
@@ -42,11 +45,14 @@ def main():
             sys.exit("Error: Empty input file")
 
         tree_obj.freeze()
-        gen = tree_obj.nearest('Parking de la', max_distance=args.max_distance)
-        if args.max_results:
-            print(list(itertools.islice(gen, args.max_results)))
-        else:
-            print(list(gen))
+        if args.query_strings:
+            for query_string in args.query_strings.split(','):
+                gen = tree_obj.nearest(query_string.strip(), max_distance=args.max_distance)
+                print('The results for query', query_string)
+                if args.max_results:
+                    print(list(itertools.islice(gen, args.max_results)))
+                else:
+                    print(list(gen))
 
 
 if __name__ == "__main__":
